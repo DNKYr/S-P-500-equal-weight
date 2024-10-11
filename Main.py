795ad54a1570fb33145cfd4d8011993ef8296d71
David Li <dtli@lcs.on.ca>
2024-10-11 14:15:15 -0400

    Calculate how much share to buy for each constitutent

--- Main.py
+++ Main.py
@@ -60,3 +60,17 @@ for stock in stocks['Symbol']:
     L.append(save_serie)
 final_dataframe = pd.DataFrame(L)
 
+#calculate how much share to bought
+portfolio_size = input("Enter the value of your portfolio")
+
+try:
+    val = float(portfolio_size)
+except ValueError:
+    print("That's not a number. \nPlease try again: ")
+    portfolio_size = input("Enter the value of your portfolio")
+    val = float(portfolio_size)
+
+position_size = val/len(final_dataframe.index)
+for i in range(0, len(final_dataframe.index)):
+    final_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[i, 'Price'])
+
