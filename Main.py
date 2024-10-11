7b6f332c141262fb79b27c39671c45e2d13aea74
David Li <dtli@lcs.on.ca>
2024-10-11 16:02:01 -0400

    Add excel sheet output and change of style.

--- Main.py
+++ Main.py
@@ -74,3 +74,43 @@ position_size = val/len(final_dataframe.index)
 for i in range(0, len(final_dataframe.index)):
     final_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[i, 'Price'])
 
+#formatting excel output
+with pd.ExcelWriter('recommended trade.xlsx') as writer:
+    final_dataframe.to_excel(writer, sheet_name = 'Recommended Trade', index = False)
+
+    background_color = '#0a0a23'
+    font_color = '#ffffff'
+
+    string_format = writer.book.add_format({
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1,
+    })
+
+    dollar_format = writer.book.add_format({
+        'num_format': '$0.00',
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1,
+    })
+
+    integer_format = writer.book.add_format({
+        'num_format': '0',
+        'font_color': font_color,
+        'bg_color': background_color,
+        'border': 1,
+    })
+
+    column_format = {
+        'A': ['Ticker', string_format],
+        'B': ['Share Price', dollar_format],
+        'C': ['Market Capitalization', dollar_format],
+        'D': ['Number of Shares to Buy', integer_format]
+    }
+
+    # Access the worksheet 'recommended trade'
+    worksheet = writer.sheets['Recommended Trade']
+
+    for column in column_format.keys():
+        worksheet.set_column(f'{column}:{column}', 18, column_format[column][1])
+        worksheet.write(f'{column}1', column_format[column][0], string_format)
